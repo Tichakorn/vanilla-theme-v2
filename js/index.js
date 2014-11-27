9049ddbfea2c71904ebfc6d1d37c759676513667
Tichakorn <tichakorn06@hotmail.com>
2014-11-27 13:59:57 +0700

    27.11.2014

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+function responsive_carousel(){
+    //set new height for responsive carousel
+    $('#fullscreen-carousel .carousel').each(function( index ) {
+        my_img=$(this).find('.carousel-image img:visible').last();
+        height = my_img.height();
+        
+        $(this).height(my_img.height());
+    });
+}
+
 $(document).ready(function () {
     if(($('#left-column').length>0)&&($('#right-column').length>0)){ 
         $( "#center-column" ).addClass( "span6" );
@@ -8,4 +18,20 @@ $(document).ready(function () {
     else if(($("#main:not(:has(#right-column))"))&&($("#main:not(:has(#right-column))"))){
         $( "#center-column" ).addClass( "span12" );
     }
+    
+    if($('#fullscreen-carousel .carousel').length>0){
+        responsive_carousel();
+    }
+    
+    $(window).resize(function() {
+        if($('#fullscreen-carousel .carousel').length>0){
+            responsive_carousel();
+        } 
+    });
+
+    $( window ).load(function() {
+        if($('#fullscreen-carousel .carousel').length>0){
+            responsive_carousel();
+        }
+    });
 });
